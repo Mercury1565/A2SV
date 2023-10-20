ad3428633520c791038675a6683aca4be6869b0a
Hermon G <78665128+Mercury1565@users.noreply.github.com>
2023-10-20 16:37:04 +0300

    Create numRescueBoats.py

---
+++ numRescueBoats.py
@@ -0,0 +1,19 @@
+class Solution:
+    def numRescueBoats(self, people: List[int], limit: int) -> int:
+        boat = 0
+        l = 0
+        r = len(people) - 1
+        people.sort()
+
+        while(l <= r):
+            if l == r:
+                boat += 1
+                return boat
+            if people[l] + people[r] <= limit:
+                l += 1
+                r -= 1
+                boat += 1
+            else:
+                r -= 1
+                boat += 1
+        return boat
