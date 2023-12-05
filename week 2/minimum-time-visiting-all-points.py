de046ff8179e3da7cde03d551d44d93d006fc413
Hermon G <78665128+Mercury1565@users.noreply.github.com>
2023-12-05 18:15:49 +0300

    Added solution for Minimum Time Visiting All Points

---
+++ week 2/minimum-time-visiting-all-points.py
@@ -0,0 +1,9 @@
+class Solution:
+    def minTimeToVisitAllPoints(self, points: List[List[int]]) -> int:
+        min_time = 0
+        for i in range(len(points) - 1):
+            min_time += max(abs(points[i][0] - points[i + 1][0]) , abs(points[i][1] - points[i + 1][1]))
+
+        return min_time
+
+        
