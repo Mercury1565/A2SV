3b65b0d6daab4abfaef5650d355ef6b794f1cd67
Hermon G <78665128+Mercury1565@users.noreply.github.com>
2023-12-12 17:24:39 +0300

    Added solution for Two Sum

---
+++ week 2/two-sum.py
@@ -0,0 +1,11 @@
+class Solution:
+    def twoSum(self, nums: List[int], target: int) -> List[int]:
+        dic = {}
+
+        for i in range(len(nums)):
+            if target - nums[i] in dic:
+                print(nums[i])
+                return [i , dic[target - nums[i]]]
+
+            dic[nums[i]] = i
+        
