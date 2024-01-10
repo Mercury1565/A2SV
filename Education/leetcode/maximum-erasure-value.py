7eb96f4ee8f7c3889b90855d451c3edfa7491c3d
Hermon G <78665128+Mercury1565@users.noreply.github.com>
2024-01-10 19:06:36 +0300

    Add solution for Maximum Erasure Value

--- Education/leetcode/maximum-erasure-value.py
+++ Education/leetcode/maximum-erasure-value.py
@@ -1,23 +1,17 @@
 class Solution:
     def maximumUniqueSubarray(self, nums: List[int]) -> int:
-        l , r = 0 , 0
-        check = set()
-        ssum = 0
-        res = 0
+        window = set()
+        score = 0
+        left = 0
 
-        while l < len(nums) and l <= r:
-            if r == len(nums):
-                res = max(res , ssum)
-                break
-                
-            if nums[r] not in check:
-                check.add(nums[r])
-                ssum += nums[r]
-                r += 1
-            else:
-                res = max(res , ssum)
-                check.remove(nums[l])
-                ssum -= nums[l]
-                l += 1
-        return res
+        for right in range(len(nums)):
+            while nums[right] in window:
+                window.remove(nums[left])
+                left += 1
 
+            window.add(nums[right])
+            score = max(score , sum(window))
+            
+        return score
+
+        
